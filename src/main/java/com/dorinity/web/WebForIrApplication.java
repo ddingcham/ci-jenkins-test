2dec74c83f5abb45f0be54f859d9f6e6e8bb9dff
seongguen <dks308@naver.com>
2018-09-24 18:07:51 +0900

    project initialized

---
+++ src/main/java/com/dorinity/web/WebForIrApplication.java
@@ -0,0 +1,12 @@
+package com.dorinity.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebForIrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebForIrApplication.class, args);
+	}
+}
