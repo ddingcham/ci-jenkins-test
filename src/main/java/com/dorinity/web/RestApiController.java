0b2a077f99ef10ceebd6842b65999763a5bab391
ddingcham <qo0op308@gmail.com>
2018-10-04 15:46:08 +0900

    add profile API

--- src/main/java/com/dorinity/web/RestApiController.java
+++ src/main/java/com/dorinity/web/RestApiController.java
@@ -1,14 +1,33 @@
 package com.dorinity.web;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class RestApiController {
+	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/")
-	public String ApiTestHandler(){
+	public String apiTestHandler(){
 		return "REST API TEST";
 	}
+	
+	@GetMapping("/profile")
+	public String showCurrentProfile(){
+		return Arrays.stream(environment.getActiveProfiles())
+				.findFirst()
+				.orElse("default profile");
+	}
+
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
+	
+	
 }
