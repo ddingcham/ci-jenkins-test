088093af788ac75f05700066e0fd892501776440
ddingcham <qo0op308@gmail.com>
2018-10-06 18:11:32 +0900

    add configuration for deployment profiles

--- src/main/java/com/dorinity/web/WebForIrApplication.java
+++ src/main/java/com/dorinity/web/WebForIrApplication.java
@@ -2,11 +2,19 @@ package com.dorinity.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WebForIrApplication {
 
+	private static final String APPLICATION_LOCATIONS =
+			"classpath:application.yml,"
+			+ "classpath:application.properties,"
+			+ "/application/config/real-application.yml";
+	
 	public static void main(String[] args) {
-		SpringApplication.run(WebForIrApplication.class, args);
+		new SpringApplicationBuilder(WebForIrApplication.class)
+		.properties(APPLICATION_LOCATIONS)
+		.run(args);
 	}
 }
